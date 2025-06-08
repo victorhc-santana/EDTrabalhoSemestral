f5e4277ce5a960dc8434ffab519b021c133b3a24
Victor Santana <victor.hc.santana@gmail.com>
2025-06-08 13:28:51 -0300

    Adiciona a classe modelo para manipulação de professor

--- src/model/Disciplina.java
+++ src/model/Disciplina.java
@@ -1,7 +1,6 @@
 package model;
 
 public class Disciplina {
-
     private int cod_disciplina;
     private int cod_curso;
     private String nome_disciplina;
---
+++ src/model/Professor.java
@@ -0,0 +1,54 @@
+package model;
+
+public class Professor {
+	private String nome_professor;
+	private String area_a_inscrever;
+	private int cpf;
+	private double pontuação;
+	
+	public Professor() {
+		
+	}
+	
+	public Professor(String nome_professor, String area_a_inscrever, int cpf, double pontuação) {
+		super();
+		this.nome_professor = nome_professor;
+		this.area_a_inscrever = area_a_inscrever;
+		this.cpf = cpf;
+		this.pontuação = pontuação;
+	}
+
+	public String getNome_professor() {
+		return nome_professor;
+	}
+
+	public void setNome_professor(String nome_professor) {
+		this.nome_professor = nome_professor;
+	}
+
+	public String getArea_a_inscrever() {
+		return area_a_inscrever;
+	}
+
+	public void setArea_a_inscrever(String area_a_inscrever) {
+		this.area_a_inscrever = area_a_inscrever;
+	}
+
+	public int getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+
+	public double getPontuação() {
+		return pontuação;
+	}
+
+	public void setPontuação(double pontuação) {
+		this.pontuação = pontuação;
+	}
+
+	
+}
